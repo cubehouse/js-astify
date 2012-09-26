d65d2ce85d8f3417f6ea4566166ee57087bba9d0
Brandon Benvie <brandon@bbenvie.com>
2012-09-26 14:09:36 -0400

    update toAST to the new stuff

--- toAST.js
+++ toAST.js
@@ -10,8 +10,11 @@ var utility  = require('./utility'),
 var ASTNode  = require('./ASTNode'),
     ASTArray = ASTNode.ASTArray,
     isNode   = ASTNode.isNode,
-    _        = ASTNode.createNode;
+    $        = ASTNode.createNode;
 
+function isNormalDesc(o){
+  return o && o.writable && o.configurable && o.enumerable;
+}
 
 module.exports = astify;
 
@@ -19,37 +22,41 @@ function astify(ctx){
   ctx = ctx || global;
   function makeAST(o, showHidden, identity, seen){
     if (!isObject(o))
-      return _('literal', o);
+      return $('#literal', o);
     else if (seen && seen.has(o))
-      return _('ident', seen.get(o));
+      return $('#ident', seen.get(o));
     else if (typeof o.toAST === 'function')
       return o.toAST(showHidden, identity, seen);
     else
       return ctx.Object.prototype.toAST.call(o, showHidden, identity, seen);
   }
 
+  var getProps = ctx.Object.getOwnPropertyNames,
+      getKeys = ctx.Object.keys,
+      describe = ctx.Object.getOwnPropertyDescriptor;
+
   function definer(o, key, name, showHidden, seen){
     if (!name && typeof o === 'function')
       name = o.name;
 
-    var args = new ASTArray([_('ident', name)]);
+    var args = new ASTArray([$('#ident', name)]);
 
     if (key instanceof Array) {
       if (key.length === 1) {
         key = key.pop();
       } else {
         var desc = {};
-        var callee = _('Object').get('defineProperties');
+        var callee = $('Object').get('defineProperties');
         key.forEach(function(key){
-          desc[key] = ctx.Object.getOwnPropertyDescriptor(o, key);
+          desc[key] = describe(o, key);
         });
       }
     }
 
     if (typeof key === 'string') {
-      var callee = _('Object').get('defineProperty');
-      var desc = ctx.Object.getOwnPropertyDescriptor(o, key);
-      args.push(_('literal', key));
+      var callee = $('Object').get('defineProperty');
+      var desc = describe(o, key);
+      args.push($('#literal', key));
     }
 
 
@@ -57,12 +64,12 @@ function astify(ctx){
       args.push(makeAST(desc, showHidden, key, seen));
       return callee.call(args);
     } else {
-      return _('literal', undefined);
+      return $('#literal', undefined);
     }
   }
 
   var primitiveToAST = function toAST(showHidden, identity, seen){
-    var out = _('literal', this);
+    var out = $('#literal', this);
     if (seen) {
       if (seen.has(this))
         return seen.get(this);
@@ -76,7 +83,11 @@ function astify(ctx){
   define(ctx.Boolean.prototype, primitiveToAST);
   define(ctx.RegExp.prototype, primitiveToAST);
 
-  define(ctx.Array.prototype, function toAST(showHidden, identity, seen){
+  function handleArgs(showHidden, identity, seen){
+    if (seen instanceof Map) {
+      seen.identity = identity;
+      return seen;
+    }
     if (typeof seen === 'boolean') {
       identity = showHidden;
       showHidden = seen;
@@ -86,135 +97,118 @@ function astify(ctx){
       identity = showHidden;
       showHidden = null;
     }
-
     seen = seen || new Map;
+    seen.hidden = showHidden;
+    seen.identity = identity;
+    return seen;
+  }
+
+  define(ctx.Array.prototype, function toAST(showHidden, identity, seen){
+    seen = handleArgs(showHidden, identity, seen);
     if (seen.has(this))
-      return _('ident', seen.get(this));
+      return $('#ident', seen.get(this));
 
-    var array = _('array');
-
-
+    var array = $('#array');
+    identity = seen.identity;
     identity && (array.identity = identity);
     seen.set(this, array.identity);
 
     this.forEach(function(item){
-      array.append(makeAST(item, showHidden, identity, seen));
+      array.append(makeAST(item, seen.hidden, identity, seen));
     });
 
-    var keys = (showHidden ? ctx.Object.getOwnPropertyNames(this) : ctx.Object.keys(this)).filter(function(k){
+    var keys = (seen.hidden ? getProps : getKeys)(this).filter(function(k){
       return isNaN(k) && k !== 'length';
     }).sort();
-    if (!keys.length) return array;
 
-    var wrapper = array.scopedDeclaration();
-    var ret = wrapper.pop();
+    if (!keys.length)
+      return array;
 
-    var descs = [];
+    var wrapper = array.scopedDeclaration(),
+        ret = wrapper.pop(),
+        descs = [];
+
     keys.forEach(function(key){
-      var desc = ctx.Object.getOwnPropertyDescriptor(this, key);
-
+      var desc = describe(this, key);
       if (desc) {
-        if ('get' in desc || 'set' in desc || !desc.enumerable || !desc.configurable || !desc.writable)
-          descs.push(key);
+        if (isNormalDesc(desc))
+          wrapper.append(ret.argument.set(key, makeAST(desc.value, seen.hidden, key, seen)));
         else
-          wrapper.append(ret.argument.set(key, makeAST(desc.value, showHidden, key, seen)));
+          descs.push(key);
       }
     }, this);
 
-    descs.length && wrapper.append(definer(this, descs, ret.argument, showHidden, seen));
+    descs.length && wrapper.append(definer(this, descs, ret.argument, seen.hidden, seen));
     wrapper.append(ret);
     return wrapper;
   });
+  var skip = ctx.Function.prototype;
 
-  define(ctx.Function.prototype, function toAST(showHidden, identity, seen){
-    if (typeof seen === 'boolean') {
-      identity = showHidden;
-      showHidden = seen;
-      seen = null;
-    }
-    if (typeof showHidden === 'string') {
-      identity = showHidden;
-      showHidden = null;
-    }
-
-    seen = seen || new Map;
+  define(skip, function toAST(showHidden, identity, seen){
+    seen = handleArgs(showHidden, identity, seen);
     if (seen.has(this))
-      return _('ident', seen.get(this));
+      return $('#ident', seen.get(this));
 
-    var func = new AST(1, this).ast;
-
+    var func = new AST(1, this).ast.body[0].expression;
+    identity = seen.identity;
     identity && (func.identity = identity);
     if (!func.identity)
       func.identity = gensym();
     seen.set(this, func.identity);
 
-
-    var skip = ctx.Function.prototype;
-    var keys = (showHidden ? ctx.Object.getOwnPropertyNames(this) : ctx.Object.keys(this)).sort().filter(function(key){
+    var keys = (seen.hidden ? getProps : getKeys)(this).filter(function(key){
       return key === 'constructor' ||
            !(key in skip || key === 'prototype' &&
-            ctx.Object.getOwnPropertyNames(this.prototype).length == 'constructor' in this.prototype);
-    }, this);
+            getProps(this.prototype).length == 'constructor' in this.prototype);
+    }, this).sort();
+
     if (!keys.length)
-      return func.body[0].expression;
+      return func;
 
-    var wrapper = func.body[0].expression.scopedDeclaration();
-    var ret = wrapper.pop();
+    var wrapper = func.scopedDeclaration(),
+        ret = wrapper.pop(),
+        descs = [];
 
-    var descs = [];
     keys.forEach(function(key){
-      var desc = ctx.Object.getOwnPropertyDescriptor(this, key);
+      var desc = describe(this, key);
       if (desc) {
-        if (key !== 'prototype' && ('get' in desc || 'set' in desc || !desc.enumerable || !desc.configurable || !desc.writable))
-          descs.push(key);
+        if (isNormalDesc(desc))
+          wrapper.append(ret.argument.set(key, makeAST(desc.value, seen.hidden, key, seen)));
         else
-          wrapper.append(ret.argument.set(key, makeAST(desc.value, showHidden, key, seen)));
+          descs.push(key);
       }
     }, this);
 
-    if (descs.length) {
-      var defs = definer(this, descs, ret.argument, showHidden, seen);
-      wrapper.append(defs);
-    }
+    descs.length && wrapper.append(definer(this, descs, ret.argument, seen.hidden, seen));
     wrapper.append(ret);
     return wrapper;
   });
 
 
   define(ctx.Object.prototype, function toAST(showHidden, identity, seen){
-    if (typeof seen === 'boolean') {
-      identity = showHidden;
-      showHidden = seen;
-      seen = null;
-    }
-    if (typeof showHidden === 'string') {
-      identity = showHidden;
-      showHidden = null;
-    }
-
-    seen = seen || new Map;
+    seen = handleArgs(showHidden, identity, seen);
     if (seen.has(this))
-      return _('ident', seen.get(this));
+      return $('#ident', seen.get(this));
 
-
-    var object = _('object');
+    var object = $('#object');
+    identity = seen.identity;
     identity && (object.identity = identity);
     seen.set(this, object.identity);
 
-    var keys = (showHidden ? ctx.Object.getOwnPropertyNames(this) : ctx.Object.keys(this)).filter(isNaN).sort();
+    var keys = (seen.hidden ? getProps : getKeys)(this).sort();
 
     keys.forEach(function(key){
       if (key === 'toString') return;
 
-      var desc = ctx.Object.getOwnPropertyDescriptor(this, key);
+      var desc = describe(this, key);
       if (desc) {
-        desc.get && object.append(_('property', 'get', key, makeAST(desc.get, showHidden, key, seen)));
-        desc.set && object.append(_('property', 'set', key, makeAST(desc.set, showHidden, key, seen)));
+        desc.get && object.append($('#property', 'get', key, makeAST(desc.get, seen.hidden, key, seen)));
+        desc.set && object.append($('#property', 'set', key, makeAST(desc.set, seen.hidden, key, seen)));
         if ('value' in desc && desc.value !== undefined && desc.value !== ASTNode) {
           try {
-            object.append(_('property', 'init', key, makeAST(desc.value, showHidden, key, seen)));
+            object.append($('#property', 'init', key, makeAST(desc.value, seen.hidden, key, seen)));
           } catch (e) {
-            console.log(desc.value)
+            console.log(e.stack)
           }
         }
       }
