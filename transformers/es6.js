fe0f3a0adbf2390b6f4c133ecb058a0f071c49e4
Brandon Benvie <brandon@bbenvie.com>
2012-09-28 01:22:02 -0400

    move transforms out of main code

--- transformers/es6.js
+++ transformers/es6.js
@@ -1,18 +1,106 @@
-var _ = require('astify').createNode;
+var ASTNode = require('astify').ASTNode,
+    $ = ASTNode.createNode,
+    converters = {},
+    freeze = $('Object').get('freeze'),
+    defaultQuasi = ASTNode.parse(function(r){
+      for (var i=0, o=''; r[i]; o += r[i].raw + (++i === r.length ? '' : arguments[i]));
+      return o;
+    });
 
-function desugar(ast, selector){
-  ast.find(selector).forEach(function(item){
-    if ('desugar' in item)
-      item.desugar();
-  });
-}
 
 module.exports = function(ast){
-  desugar(ast, 'class');
-  desugar(ast, 'module');
-  desugar(ast, 'taggedquasi');
-  desugar(ast, 'quasi');
-  desugar(ast, 'arrow');
+  ['class', 'module', 'arrow', 'taggedquasi', 'quasi'].forEach(function(selector){
+    ast.find(selector).forEach(function(item){
+      converters[selector](item);
+    });
+  });
   return ast;
 }
 
+
+
+function addConverter(type, callback){
+  converters[type] = callback;
+}
+
+
+addConverter('class', function(node){
+  var ctor = node.findConstructor();
+
+  ctor.id = node.id || node.identity;
+
+  if (node.superClass) {
+    var superclass = node.superClass;
+    node.find('member[object=super]').forEach(function(superCall){
+      var call = superCall.getParent();
+      call.callee = superclass.get('prototype').get(superCall.property);
+      call.call();
+    });
+    node.find('call[callee=super]').forEach(function(superCall){
+      superCall.callee = superclass.clone();
+      superCall.call();
+    });
+  }
+
+  var closure = ctor.scopedDeclaration();
+  var ret = closure.pop();
+
+  if (node.superClass)
+    closure.addArgument(node.superClass);
+
+
+  var prototype = $('#object', { constructor: node.id });
+  closure.append(node.id.set('prototype', prototype));
+
+  node.findAccessors().forEach(function(accessor){
+    prototype.append($('#property', accessor.kind, accessor.key, accessor.value));
+  });
+  node.findMethods().forEach(prototype.set.bind(prototype));
+
+
+  if (node.type === 'ClassDeclaration') {
+    var decl = closure.declaration(ctor.id);
+    decl.identity = ctor.id.name;
+    node.replaceWith(decl);
+  } else {
+    node.replaceWith(closure);
+  }
+});
+
+addConverter('arrow', function(node){
+  node.replaceWith(node.toExpression());
+});
+
+addConverter('module', function(node){
+  node.find('export').forEach(converters.export);
+  var closure = $('#functionexpr', null, ['global', 'exports'], node.body);
+  var args = [$('#this'), $('#this'), ASTNode.parse('typeof exports === "undefined" ? {} : exports')];
+  closure.returns('exports');
+  node.replaceWith(freeze.call(closure.get('call').call(args)).declaration(node.id));
+});
+
+addConverter('quasi', function(node, tag){
+  var components = $('#array'),
+      identity = $(node.identity),
+      params = node.expressions.clone();
+
+  for (var i=0; i < node.quasis.length; i++) {
+    components.append(freeze.call($('#object', node.quasis[i].value)));
+  }
+
+  node.parentScope().declare(node.identity);
+  params.unshift(identity.OR(identity.SET(freeze.call(components))));
+  node.replaceWith((tag || defaultQuasi).clone().call(params));
+});
+
+addConverter('taggedquasi', function(node){
+  converters.quasi(node.quasi, node.tag);
+  node.replaceWith(node.quasi);
+});
+
+addConverter('export', function(node){
+  var decl = node.declaration.declarations[0],
+      exports = $('exports').set(decl.id, decl.init.clone());
+  decl.init.replaceWith(exports);
+  node.replaceWith(node.declaration);
+});
--- transformers/operators.js
+++ transformers/operators.js
@@ -1,118 +1,221 @@
 var ASTNode = require('astify').ASTNode,
+    $ = ASTNode.createNode,
     ASTArray = ASTNode.ASTArray;
 
-var funcs = {},
-    runtimeNames = Object.create(null);
+var handlers = {};
 
-function get(type){
-  var ops = ASTNode.types[type][0].operators;
-  Object.keys(ops).forEach(function(key){
-    funcs[key] = ASTNode.parse(ops[key]);
-    runtimeNames[funcs[key].id.name] = key;
-  });
+function register(type, callback){
+  handlers[type] = callback;
 }
 
-get('binary');
-get('logical');
-get('unary');
-get('update');
-get('assign');
+register('update', function(node){
+  var name = node.prefix ? '_' + node.operator : node.operator + '_';
+  var call = $(names.update[name]).call([node.argument]);
+  node.replaceWith(node.argument.get('#assign', '=', call));
+});
 
-module.exports = overload;
+register('unary', function(node){
+  node.replaceWith(node.argument.get(names.unary[node.operator]).call([]));
+});
 
-function overload(ast){
-  ast.find('class').forEach(function(subject){
-    var overloads = {};
-    subject.find('method').forEach(function(method){
-      if (method.key.name in runtimeNames) {
-        overloads[method.key.name] = true;
-      }
-    });
+register('logical', function(node){
+  node.replaceWith(node.left.get(names.logical[node.operator]).call([node.right]));
+});
 
-      console.log(overloads)
-  });
-  return ast;
-}
+register('binary', function(node){
+  node.replaceWith(node.left.get(names.binary[node.operator]).call([node.right]));
+});
 
-function convertAllOperators(ast){
-  var used = {};
+register('assign', function(node){
+  var call = node.left.get(names.assign[node.operator]).call([node.right]);
+  node.replaceWith($('#assign', '=', node.left.clone(), call));
+});
 
-  ['binary', 'logical', 'unary', 'update', 'assign'].forEach(function(type){
+
+module.exports = function(ast){
+  for (var type in handlers) {
     ast.find(type).forEach(function(expr){
-      used[expr.operator] = true;
-      expr.becomeCall();
+      var scope = expr.parentScope();
+      if (scope) {
+        name = scope.id || scope.key;
+        if (name && name.name in ops)
+          return;
+      }
+      handlers[type](expr);
     });
-  });
+  }
 
-  var functions = new ASTArray;
-  Object.keys(used).sort().forEach(function(operator){
-    if (operator in funcs)
-      functions.append(funcs[operator]);
-    else
-      console.log(operator)
-  });
-
-  ast.prepend(functions);
   return ast;
 }
 
-var operators = {
-// numeric
-function ADD(r){ return this + r }
-function SUBTRACT(r){ return this - r }
-function DIVIDE(r){ return this / r }
-function MULTIPLY(r){ return this * r }
-function MOD(r){ return this % r }
-// bitwise
-function BIT_XOR(r){ return this ^ r }
-function BIT_AND(r){ return this & r }
-function BIT_OR(r){ return this | r }
-function RIGHT_SHIFT(r){ return this >> r }
-function LEFT_SHIFT(r){ return this << r }
-function SIGNED_RIGHT_SHIFT(r){ return this >>> r }
-function BIT_NOT(){ return ~this }
-function TO_NUMBER(){ return +this }
-function NEGATE(){ return -this }
-// boolean
-function IS_EQUAL(r){ return this === r }
-function IS_SIMILAR(r){ return this == r }
-function IS_GREATER(r){ return this > r }
-function IS_LESS(r){ return this < r }
-function IS_INEQUAL(r){ return this !== r }
-function IS_DIFFERENT(r){ return this != r }
-function IS_EQ_OR_GREATER(r){ return this >= r }
-function IS_EQ_OR_LESS(r){ return this <= r }
-// logical
-function NOT(){ return !this }
-function OR(r){ return this || r }
-function AND(r){ return this && r }
-// mutate
-function SET(r){ return this = r }
-function SET_MULTIPLY(r){ return this *= r }
-function SET_DIVIDE(r){ return this /= r }
-function SET_MOD(r){ return this %= r }
-function SET_ADD(r){ return this += r }
-function SET_SUBTRACT(r){ return this -= r }
-function SET_LEFT_SHIFT(r){ return this <<= r }
-function SET_RIGHT_SHIFT(r){ return this >>= r }
-function SET_SIGNED_RIGHT_SHIFT(r){ return this >>>= r }
-function SET_AND(r){ return this &= r }
-function SET_XOR(r){ return this ^= r }
-function SET_OR(r){ return this |= r }
-function PRE_INCREMENT(){ return this.SET(this.ADD(1)) }
-function PRE_DECREMENT(){ return this.SET(this.SUBTRACT(1)) }
-function POST_INCREMENT(){ var c = this.CLONE(); this.PRE_INCREMENT(); return c; }
-function POST_DECREMENT(){ var c = this.CLONE(); this.PRE_DECREMENT(); return c; }
-// other
-function VOID(){ return void this }
-function TYPEOF(){ return typeof this }
-function IS_IN(r) { return this in r }
-function IS_INSTANCE(r) { return this instanceof r }
-function DELETE(r) { return delete this[r] }
+var ops = {
+  SET: true,
+  SET_MULTIPLY: true,
+  SET_DIVIDE: true,
+  SET_MOD: true,
+  SET_ADD: true,
+  SET_SUBTRACT: true,
+  SET_LEFT_SHIFT: true,
+  SET_RIGHT_SHIFT: true,
+  SET_SIGNED_RIGHT_SHIFT: true,
+  SET_AND: true,
+  SET_XOR: true,
+  SET_OR: true,
+  ADD: true,
+  SUBTRACT: true,
+  DIVIDE: true,
+  MULTIPLY: true,
+  MOD: true,
+  BIT_XOR: true,
+  BIT_AND: true,
+  BIT_OR: true,
+  RIGHT_SHIFT: true,
+  LEFT_SHIFT: true,
+  SIGNED_RIGHT_SHIFT: true,
+  IS_EQUAL: true,
+  IS_SIMILAR: true,
+  IS_GREATER: true,
+  IS_LESS: true,
+  IS_INEQUAL: true,
+  IS_DIFFERENT: true,
+  IS_EQ_OR_GREATER: true,
+  IS_EQ_OR_LESS: true,
+  IS_IN: true,
+  DELETE: true,
+  IS_INSTANCE: true,
+  OR: true,
+  AND: true,
+  NOT: true,
+  NEGATE: true,
+  TO_NUMBER: true,
+  BIT_NOT: true,
+  VOID: true,
+  TYPEOF: true,
+  INCREMENT: true,
+  DECREMENT: true,
+  PRE_INCREMENT: true,
+  PRE_DECREMENT: true,
+  POST_INCREMENT: true,
+  POST_DECREMENT: true,
+};
+
+var names = {
+  assign: {
+    '='           : 'SET',
+    '*='          : 'SET_MULTIPLY',
+    '/='          : 'SET_DIVIDE',
+    '%='          : 'SET_MOD',
+    '+='          : 'SET_ADD',
+    '-='          : 'SET_SUBTRACT',
+    '<<='         : 'SET_LEFT_SHIFT',
+    '>>='         : 'SET_RIGHT_SHIFT',
+    '>>>='        : 'SET_SIGNED_RIGHT_SHIFT',
+    '&='          : 'SET_AND',
+    '^='          : 'SET_XOR',
+    '|='          : 'SET_OR',
+  },
+  binary: {
+    '+'           : 'ADD',
+    '-'           : 'SUBTRACT',
+    '/'           : 'DIVIDE',
+    '*'           : 'MULTIPLY',
+    '%'           : 'MOD',
+    '^'           : 'BIT_XOR',
+    '&'           : 'BIT_AND',
+    '|'           : 'BIT_OR',
+    '>>'          : 'RIGHT_SHIFT',
+    '<<'          : 'LEFT_SHIFT',
+    '>>>'         : 'SIGNED_RIGHT_SHIFT',
+    '==='         : 'IS_EQUAL',
+    '=='          : 'IS_SIMILAR',
+    '>'           : 'IS_GREATER',
+    '<'           : 'IS_LESS',
+    '!=='         : 'IS_INEQUAL',
+    '!='          : 'IS_DIFFERENT',
+    '>='          : 'IS_EQ_OR_GREATER',
+    '<='          : 'IS_EQ_OR_LESS',
+    'in'          : 'IS_IN',
+    'delete'      : 'DELETE',
+    'instanceof'  : 'IS_INSTANCE',
+  },
+  logical: {
+    '||'          : 'OR',
+    '&&'          : 'AND',
+  },
+  unary: {
+    '!'           : 'NOT',
+    '~'           : 'NEGATE',
+    '+'           : 'TO_NUMBER',
+    '-'           : 'BIT_NOT',
+    'void'        : 'VOID',
+    'typeof'      : 'TYPEOF',
+  },
+  update: {
+    '++'          : 'INCREMENT',
+    '--'          : 'DECREMENT',
+    '_++'         : 'PRE_INCREMENT',
+    '_--'         : 'PRE_DECREMENT',
+    '++_'         : 'POST_INCREMENT',
+    '--_'         : 'POST_DECREMENT',
+  }
+};
 
 
-
-
+function install(){
+  [ function ADD(r){ return this + r },
+    function SUBTRACT(r){ return this - r },
+    function DIVIDE(r){ return this / r },
+    function MULTIPLY(r){ return this * r },
+    function MOD(r){ return this % r },
+    function BIT_XOR(r){ return this ^ r },
+    function BIT_AND(r){ return this & r },
+    function BIT_OR(r){ return this | r },
+    function RIGHT_SHIFT(r){ return this >> r },
+    function LEFT_SHIFT(r){ return this << r },
+    function SIGNED_RIGHT_SHIFT(r){ return this >>> r },
+    function BIT_NOT(){ return ~this },
+    function TO_NUMBER(){ return +this },
+    function NEGATE(){ return -this },
+    function IS_EQUAL(r){ return this === r },
+    function IS_SIMILAR(r){ return this == r },
+    function IS_GREATER(r){ return this > r },
+    function IS_LESS(r){ return this < r },
+    function IS_INEQUAL(r){ return this !== r },
+    function IS_DIFFERENT(r){ return this != r },
+    function IS_EQ_OR_GREATER(r){ return this >= r },
+    function IS_EQ_OR_LESS(r){ return this <= r },
+    function NOT(){ return !this },
+    function OR(r){ return this || r },
+    function AND(r){ return this && r },
+    function SET(r){ return r },
+    function SET_MULTIPLY(r){ return this * r },
+    function SET_DIVIDE(r){ return this / r },
+    function SET_MOD(r){ return this % r },
+    function SET_ADD(r){ return this + r },
+    function SET_SUBTRACT(r){ return this - r },
+    function SET_LEFT_SHIFT(r){ return this << r },
+    function SET_RIGHT_SHIFT(r){ return this >> r },
+    function SET_SIGNED_RIGHT_SHIFT(r){ return this >>> r },
+    function SET_AND(r){ return this & r },
+    function SET_XOR(r){ return this ^ r },
+    function SET_OR(r){ return this | r },
+    function PRE_INCREMENT(){ return this.SET(this.ADD(1)) },
+    function PRE_DECREMENT(){ return this.SET(this.SUBTRACT(1)) },
+    function POST_INCREMENT(){ var c = this.CLONE(); this.PRE_INCREMENT(); return c; },
+    function POST_DECREMENT(){ var c = this.CLONE(); this.PRE_DECREMENT(); return c; },
+    function VOID(){ return void this },
+    function TYPEOF(){ return typeof this },
+    function IS_IN(r) { return this in r },
+    function IS_INSTANCE(r) { return this instanceof r },
+    function DELETE(r) { return delete this[r] }
+  ].forEach(function(operator){
+    Object.defineProperty(Object.prototype, {
+      value: operator,
+      configurable: true
+    });
+  });
+}
+/*
 
 // derived versions
 function TO_NUMBER(){ return this.valueOf() }
@@ -138,3 +241,4 @@ function PRE_INCREMENT(){ return this.SET(this.ADD(1)) }
 function PRE_DECREMENT(){ return this.SET(this.SUBTRACT(1)) }
 function POST_INCREMENT(){ var c = this.CLONE(); this.PRE_INCREMENT(); return c; }
 function POST_DECREMENT(){ var c = this.CLONE(); this.PRE_DECREMENT(); return c; }
+*/
