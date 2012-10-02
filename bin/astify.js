5f39172fe2bad6d9ce59832c7a49cbbb9c40cd61
Brandon Benvie <brandon@bbenvie.com>
2012-10-01 21:54:26 -0400

    change the bin file to be extensible with ExecutionProfile and Transform

--- bin/astify.js
+++ bin/astify.js
@@ -2,70 +2,141 @@ var fs = require('fs'),
     path = require('path'),
     astify = require('astify');
 
+module.exports = function load(){
+  return new ExecutionProfile(arguments);
+};
 
-var paths = [];
-var selected = [];
-var transformLocation = path.resolve(__dirname, '..', 'transformers');
-var transformers = {};
-fs.readdirSync(transformLocation).map(function(name){
-  if (path.extname(name) === '.js')
-    transformers[name.slice(0, -3)] = path.resolve(transformLocation, name);
-});
+module.exports.Transform = Transform;
+module.exports.ExecutionProfile = ExecutionProfile;
 
-function addTransforms(array){
-  if (array instanceof Array && array.length) {
-    array.forEach(function(item){
-      if (item in transformers) {
-        selected.push(require(transformers[item]));
-      }
-    });
+
+function Transform(directory, name){
+  this.dirname = directory;
+  this.filename = require.resolve(path.resolve(directory, name));
+  this.name = path.basename(name);
+  this.transform = require(this.filename);
+}
+
+Transform.location = path.resolve(__dirname, '..', 'transformers');
+
+Transform.transformers = {};
+
+Transform.load = function load(name){
+  if (name in Transform.transformers) {
+    if (!(Transform.transformers[name] instanceof Transform))
+      Transform.transformers[name] = new Transform(Transform.location, name)
+    return Transform.transformers[name];
   }
 }
 
-var run;
-
-var args = process.argv.filter(function(arg){
-  if (arg === '-r') {
-    run = true;
-    return;
-  }
-  arg = path.resolve(arg);
-  if (arg !== __filename && arg !== process.execPath) {
-    if (fs.existsSync(arg))
-      paths.push(arg);
-    else
-      return true;
-  }
+fs.readdirSync(Transform.location).map(function(name){
+  if (path.extname(name) === '.js')
+    Transform.transformers[name.slice(0, -3)] = true;
 });
 
-if (!paths.length) return;
-
-addTransforms(args);
-
-var input = paths.shift(),
-    output = paths.shift();
-
-var src = fs.readFileSync(input, 'utf8'),
-    ast = astify.ASTNode.parse(src),
-    inline = src.match(/^\/\/!#(.*?)\n/);
-
-inline && addTransforms(inline[1].split('|'));
 
 
-selected.forEach(function(transform){
-  ast = transform(ast);
-});
+function ExecutionProfile(args){
+  var paths = [];
+  this.commands = [];
+  this.transforms = [];
+  [].forEach.call(args || [], function(arg){
+    if (arg in ExecutionProfile.commands)
+      return this.commands.push(ExecutionProfile.commands[arg])
 
-src = ast.toSource();
+    arg = path.resolve(arg);
+    if (arg !== __filename && arg !== process.execPath) {
+      if (fs.existsSync(arg))
+        paths.push(arg);
+      else
+        this.addTransforms(item);
+    }
+  }, this);
 
-if (output && path.extname(output) === '.js' && fs.existsSync(path.dirname(output))) {
-  fs.writeFileSync(output, src);
-} else if (run) {
-  fs.writeFileSync('__temp.js', src);
-  var child = require('child_process').spawn(process.execPath, [path.resolve('__temp.js')], { stdio: 'inherit' });
-  child.on('close', function(){
-    fs.unlinkSync('__temp.js');
+  if (paths.length)
+    this.input = paths.shift();
+  if (paths.length)
+    this.output = paths.shift();
+}
+
+ExecutionProfile.commands = {
+  run: function(profile){
+    profile.execute();
+  },
+  save: function(profile){
+    profile.save();
+  },
+  log: function(profile){
+    console.log(profile.transformed.toSource())
+  }
+};
+
+ExecutionProfile.commands['-r'] = ExecutionProfile.commands.run;
+ExecutionProfile.commands['-s'] = ExecutionProfile.commands.save;
+
+
+ExecutionProfile.prototype.load = function load(file){
+  file = file || this.input;
+  this.src = fs.readFileSync(file, 'utf8');
+  this.ast = astify.ASTNode.parse(this.src);
+  var inline = this.src.match(/^\/\/!#(.*?)\n/);
+  inline && this.addTransforms(inline[1].split('|'));
+  return this;
+};
+
+ExecutionProfile.prototype.addTransforms = function addTransforms(transforms){
+  if (typeof transforms === 'string' || typeof transforms === 'function')
+    transforms = [transforms];
+
+  if (transforms instanceof Array) {
+    transforms.forEach(function(transform){
+      if (typeof transform === 'string') {
+        transform = Transform.load(transform);
+        if (transform)
+          this.transforms.push(transform);
+      } else if (typeof transform === 'function') {
+        this.transforms.push(transform);
+      }
+    }, this);
+  }
+  return this;
+};
+
+ExecutionProfile.prototype.transform = function transform(){
+  return this.transformed = this.transforms.reduce(function(ast, transformer){
+    return transformer.transform(ast.clone());
+  }, this.ast);
+};
+
+ExecutionProfile.prototype.toSource = function toSource(){
+  return (this.transformed || this.transform()).toSource();
+};
+
+ExecutionProfile.prototype.execute = function execute(){
+  var temp = path.resolve('__temp.js');
+  fs.writeFileSync(temp, this.toSource());
+  this.child = require('child_process').spawn(process.execPath, [temp], { stdio: 'inherit' });
+  this.child.on('close', function(){
+    fs.unlinkSync(temp);
   });
-} else {
-  console.log(src);
+  return this.child;
+};
+
+ExecutionProfile.prototype.save = function save(file){
+  file = file || this.output;
+  if (path.existsSync(path.dirname(file)))
+    fs.writeFileSync(file, this.toSource());
+  return this;
+};
+
+
+if (process.mainModule === module) {
+  var exec = new ExecutionProfile(process.argv).load();
+  if (!exec.commands.length)
+    exec.commands.push(ExecutionProfile.commands.log);
+
+  exec.transform();
+  exec.commands.forEach(function(command){
+    command(exec);
+  });
 }
